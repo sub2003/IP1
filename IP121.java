09fcbf773e55b080fa9abd4f04c2f80129b3b153
sub2003 <abcsraththanayaka21@gmail.com>
2025-05-23 21:19:48 +0530

    Create IP121.java

---
+++ IP121.java
@@ -0,0 +1,22 @@
+import java.util.Scanner; 
+
+public class IP121 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+        
+		System.out.print("Enter the length of sqaure : ");
+		Double num1=sc.nextDouble(); 
+		
+	  
+		Double c=num1*4;
+		  Double r =c/(3.14*2);
+		
+	
+		
+		System.out.print("The rsdius is : "+r);
+		
+	}
+
+}
